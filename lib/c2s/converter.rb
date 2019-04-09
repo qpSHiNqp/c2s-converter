4cada29a3287c31a3aee60400c04d48a0cfee416
田中晋太朗 <shintaro.tanaka@onecareer.jp>
2019-04-09 11:56:59 +0900

    First commit

---
+++ lib/c2s/converter.rb
@@ -0,0 +1,8 @@
+require "c2s/converter/version"
+
+module C2s
+  module Converter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/c2s/converter/version.rb
@@ -0,0 +1,5 @@
+module C2s
+  module Converter
+    VERSION = "0.1.0"
+  end
+end
