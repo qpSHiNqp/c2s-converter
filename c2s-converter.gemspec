94676e37c1036145be820c815f66d5897f06a184
Shintaro Tanaka <tallow@dirt.ninja>
2019-04-20 14:39:37 +0900

    File archiver

--- c2s-converter.gemspec
+++ c2s-converter.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
 
   spec.add_runtime_dependency "chatwork_to_slack"
+  spec.add_runtime_dependency "nokogiri"
 end
--- lib/c2s/attachment.rb
+++ lib/c2s/attachment.rb
@@ -1,9 +1,52 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
 module C2s
   class Attachment
+    attr_accessor :input_dir, :output_dir, :token
     def initialize(input_dir, output_dir)
+      @input_dir = input_dir
+      @output_dir = output_dir
+      @token = ENV['CW_TOKEN']
     end
 
     def run
+      base = Pathname.new(input_dir)
+      Dir[File.join(input_dir, "**", "*")].each do |absolute_path|
+        next unless FileTest.file? absolute_path
+        file = Pathname.new(absolute_path).relative_path_from(base)
+        if file.to_s =~ /file\.html/
+          doc = Nokogiri::HTML.parse(File.read(absolute_path), nil)
+          room_id = File.basename(File.dirname(absolute_path))
+          room = nil
+          doc.xpath('//h1[@class="autotrim"]').each do |room_node|
+            room = room_node.inner_text
+            room.gsub!(/\//, "_") if room =~ /\//
+          end
+          doc.xpath('//tbody/tr').each do |node|
+            anchor = node.css('td.d-chat_list-file-name span a')
+            fid = anchor.attribute('href').text().match(/fid=(\d+)$/)[1]
+            name = anchor.inner_text
+            user = node.css('td.d-chat_list-file-user span').inner_text
+            user.gsub!(/\s/, "") if user =~ /\s/
+            time = node.css('td.d-chat_list-file-update').inner_text.gsub!(/\s/, "")
+            filename = [time, user, name].join("__")
+            filename.gsub!(/\//, "_") if filename =~ /\//
+            res =  open("https://api.chatwork.com/v2/rooms/#{room_id}/files/#{fid}?create_download_url=1", "X-ChatWorkToken" => token).read
+            info = JSON.parse(res)
+            output_filename = File.join(output_dir, room, filename)
+            FileUtils.mkdir_p(File.dirname(output_filename))
+            open(output_filename, "wb") do |output|
+              open(info["download_url"]) do |data|
+                output.write(data.read)
+              end
+            end
+            puts "Saved #{output_filename}"
+          end
+          puts "Processed #{room} (#{room_id})"
+        end
+      end
     end
   end
 end
